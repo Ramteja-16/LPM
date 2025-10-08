d58dac64b683c21f6cdf03c6fa1056eb0380964b
SURAMPUDI RAM TEJA <155452769+Ramteja-16@users.noreply.github.com>
2025-10-08 10:01:07 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,11 @@ const BASE_URL="https://api.themoviedb.org/3"
 
 export const getPopularMovies = async() => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    
+     if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+    
     const data = await response.json()
     return data.results
 };
@@ -11,6 +16,11 @@ export const searchMovies = async(query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
         query
     )}`);
+
+    if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+    
     const data = await response.json()
     return data.results
 };
